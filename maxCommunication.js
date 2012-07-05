15c8953ec0fe854a3007981c701a654e6dab8e4c
Florian Demmer <florian@cycling74.com>
2012-07-05 14:07:47 -0700

    Made maxCommunicator to a Singleton module

--- maxCommunication.js
+++ maxCommunication.js
@@ -1,92 +1,95 @@
-var MaxCommunication = function(socketHost,udpPort){
-	this.socket = Websocket.getInstance({'host' : socketHost});
-	this.udpPort = udpPort;
-	this.controls = {};
-	this.byteConverter = ByteConverter.getInstance();
-	this.oscLib = LibOSC.getInstance();
+var MaxCommunication = (function(){
+    var instantiated;
 
-	this.instantiateSocket(this.oscLib,this.controls); 			// workaround in order to pass instance variables to socket object
-	this.setDefaultBehaviour(function(msg){console.log(msg);}); // if no default function is set just log the entire msg to the console
-};
+    function init(options){
 
-MaxCommunication.prototype.instantiateSocket = function(oscLib,controls){
-	this.socket.receiveMsg("max",function(data){
-		var oscMsg = oscLib.parseOSCMsg(data);
-		var functionId = oscMsg.address;
+    	var socket = Websocket.getInstance({'host' : options.socketHost});
+		var udpPort = options.udpPort;
 
-		if(controls[functionId] != undefined){
-			controls[functionId](oscMsg.typeFlags,oscMsg.values);
-		} else{
-			controls["default"](oscMsg);
+		var byteConverter = ByteConverter.getInstance();
+		var oscLib = LibOSC.getInstance();
+
+		var controls = {
+				'default' : function(msg){console.log(msg);}
+		};
+
+		// add Max message listener and handling to socket
+		socket.receiveMsg("max",function(data){
+			var oscMsg = oscLib.parseOSCMsg(data);
+			var functionId = oscMsg.address;
+
+			if(controls[functionId] != undefined){
+				controls[functionId](oscMsg.typeFlags,oscMsg.values);
+			} else{
+				controls["default"](oscMsg);
+			}
+		});
+
+		// private function to send a message to Max
+		function sendMessageToMax(address,typeArray,valueArray){
+			socket.sendMsg("max",{"port" : udpPort, "mssg" : oscLib.createOSCMsg(address,typeArray,valueArray)});
 		}
-	});
-};
 
-/** ----- USE FOR SENDING TO SERVER ----- */
+		return {
+			// SERVER ONLY COMMUNICATION
+			sendMsgToServer : function(msgType,data){
+				socket.sendMsg(msgType,data);
+			},
+			receiveMsgFromServer : function(msgType,operation){
+				socket.receiveMsg(msgType,operation);
+			},
+			// RECEIVING MSGS FROM MAX
+			setDefaultBehaviour : function(fct){
+				controls["default"] = fct;
+			},
+			addMaxMsgHandler : function(address,operation){
+				controls[address] = operation;
+			},
+			// SENDING MSGS TO MAX
+			sendMsgToMax : function(address,typeArray,valueArray){
+				sendMessageToMax(address,typeArray,valueArray);
+			},
+			sendStringToMax : function(address,value){
+				sendMessageToMax(address,["s"],[value]);
+			},
+			sendStringsToMax : function(address,valueArray){
+				var typeArray = new Array();
+				for(var i = 0; i < valueArray.length; i++){
+					typeArray.push("s");
+				}
+				sendMessageToMax(address,typeArray,valueArray);
+			},
+			sendIntToMax : function(address,value){
+				sendMessageToMax(address,["i"],[value])
+			},
+			sendIntsToMax : function(address,valueArray){
+				var typeArray = new Array();
+				for(var i = 0; i < valueArray.length; i++){
+					typeArray.push("i");
+				}
+				sendMessageToMax(address,typeArray,valueArray);
+			},
+			sendFloatToMax : function(address,value){
+				sendMessageToMax(address,["f"],[value]);
+			},
+			sendFloatsToMax : function(address,valueArray){
+				var typeArray = new Array();
+				for(var i = 0; i < valueArray.length; i++){
+					typeArray.push("f");
+				}
+				sendMessageToMax(address,typeArray,valueArray);
+			}
+		}; // END OF PUBLIC SPACE
+    }
 
-MaxCommunication.prototype.sendMsgToServer = function(msgType,data){
-	this.socket.sendMsg(msgType,data);
-};
+    // Singleton behaviour
+    return {
+    	getInstance : function(options){
+			if (!instantiated){
+				instantiated = init(options);
+			}
+			return instantiated; 
+		}
+    };
 
-/** ----- USE FOR RECEIVING FROM SERVER ----- **/
-
-MaxCommunication.prototype.receiveMsgFromServer = function(msgType,operation){
-	this.socket.receiveMsg(msgType,operation);
-};
-
-/** ----- USE FOR RECEIVING MESSAGES FROM MAX ----- **/
-
-MaxCommunication.prototype.setDefaultBehaviour = function(fct){
-	this.controls["default"] = fct;
-};
-
-MaxCommunication.prototype.addMaxMsgHandler = function(address,operation){
-	this.controls[address] = operation;
-};
-
-/** ----- USE FOR SENDING MESSAGES TO MAX ----- **/
-
-MaxCommunication.prototype.sendMsgToMax = function(address,typeArray,valueArray){
-	this.socket.sendMsg("max",{"port" : this.udpPort, "mssg" : this.oscLib.createOSCMsg(address,typeArray,valueArray)});
-};
-
-MaxCommunication.prototype.sendStringToMax = function(address,value){
-	this.sendMsgToMax(address,["s"],[value]);
-};
-
-MaxCommunication.prototype.sendStringsToMax = function(address,valueArray){
-	var typeArray = new Array();
-	for(var i = 0; i < valueArray.length; i++){
-		typeArray.push("s");
-	}
-	this.sendMsgToMax(address,typeArray,valueArray);
-};
-
-MaxCommunication.prototype.sendIntToMax = function(address,value){
-	this.sendMsgToMax(address,["i"],[value])
-};
-
-MaxCommunication.prototype.sendIntsToMax = function(address,valueArray){
-	var typeArray = new Array();
-	for(var i = 0; i < valueArray.length; i++){
-		typeArray.push("i");
-	}
-	this.sendMsgToMax(address,typeArray,valueArray);
-};
-
-MaxCommunication.prototype.sendFloatToMax = function(address,value){
-	this.sendMsgToMax(address,["f"],[value]);
-};
-
-MaxCommunication.prototype.sendFloatsToMax = function(address,valueArray){
-	var typeArray = new Array();
-	for(var i = 0; i < valueArray.length; i++){
-		typeArray.push("f");
-	}
-	this.sendMsgToMax(address,typeArray,valueArray);
-};
-
-
-
-
-
+})();
